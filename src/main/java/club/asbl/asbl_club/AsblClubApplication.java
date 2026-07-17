0969b6f66183fd652490556e87e208586fa169c1
Yassin Hassan <ynhassan22@gmail.com>
2026-07-17 22:38:07 +0200

    initial commit

---
+++ src/main/java/club/asbl/asbl_club/AsblClubApplication.java
@@ -0,0 +1,13 @@
+package club.asbl.asbl_club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsblClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsblClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/asbl/asbl_club/AsblClubApplicationTests.java
@@ -0,0 +1,13 @@
+package club.asbl.asbl_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsblClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
