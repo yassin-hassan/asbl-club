972c36ccb6431f05ce1480cae6139b26a7f2fd3d
Yassin Hassan <ynhassan22@gmail.com>
2026-07-23 12:45:14 +0200

    feat(event): add a scoped lookup of an event by id

--- src/main/java/club/asbl/asbl_club/event/EventRepository.java
+++ src/main/java/club/asbl/asbl_club/event/EventRepository.java
@@ -2,9 +2,12 @@ package club.asbl.asbl_club.event;
 
 import club.asbl.asbl_club.asbl.Asbl;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findByAsbl(Asbl asbl);
+
+    Optional<Event> findByIdAndAsbl(Long id, Asbl asbl);
 }
